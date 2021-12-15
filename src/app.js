cb9c634a55b0c880c8cc07e2e86f78454fcc77c8
Muhammad Musab <yaqoobmusab03@gmail.com>
2021-12-15 14:05:56 -0600

    add getOriginPlaces funtion and import the mapApi, bBox and transitApi

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
+import { transitApi, mapApi, bBox } from "./modules/api.js";
+
 const originForm = document.querySelector('.origin-form');
+const originUL = document.querySelector('.origins');
 
 originForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const input = e.target[0].value
   if (input !== '') {
-    console.log(input)
+    getOriginPlaces(input)
+    originUL.innerHTML = ''
   } else {
     return;
   }
 });
+
+function getOriginPlaces(name) {
+  fetch(`${mapApi.url}/${name}.json?bbox=${bBox.minLon},${bBox.minLat},${bBox.maxLong},${bBox.maxLat}&limit=10&access_token=${mapApi.key}`)
+  .then(response => response.json())
+  .then(data => {
+    if (data.features.length === 0) {
+      const error = document.createElement('DIV');
+      error.innerHTML = 'Sorry this place does not exist'
+      originUL.appendChild(error)
+      return;
+    }
+    data.features.forEach(place => {
+      console.log(place)
+    })
+  })
+}
