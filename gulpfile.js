663189c0bcca29a52deec11007c3577f44e87820
Muhammad Musab <yaqoobmusab03@gmail.com>
2021-12-15 13:44:37 -0600

    add starter files

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const {src, dest, parallel, watch} = require('gulp');
+const gulpWebpack = require('webpack-stream')
+
+function pagesTask() {
+  return src('src/*.html').pipe(dest('dist'))
+}
+
+function scriptsTask() {
+  return src('src/**/*.js')
+  .pipe(gulpWebpack({mode: 'production'}))
+  .pipe(dest('dist'))
+}
+
+function stylesTask() {
+  return src('src/*.css').pipe(dest('dist'))
+}
+
+function watchTask() {
+  return watch(['src/*'], parallel(pagesTask, scriptsTask, stylesTask))
+}
+
+exports.default = parallel(pagesTask, scriptsTask, stylesTask)
+exports.watch = watchTask
