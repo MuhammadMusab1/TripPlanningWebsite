fab27075f53d39ac47557d51a71332f5ce4a2c16
Muhammad Musab <yaqoobmusab03@gmail.com>
2021-12-15 15:32:59 -0600

    add and export createSegmentObj in makeObj.js

--- src/modules/makeObj.js
+++ src/modules/makeObj.js
@@ -6,3 +6,31 @@ export function createPlaceObj(place) {
     latitude: place.center[1]  
   }
 }
+
+export function createSegmentObj(seg) {
+  if (seg.type === 'walk') {
+    return {
+      type: seg.type,
+      time : seg.times.durations.total,
+      stopNum: seg.to === undefined  || seg.to.stop === undefined? 'none' : seg.to.stop.key ,
+      stopName: seg.to === undefined || seg.to.stop === undefined? 'destination' : seg.to.stop.name
+    }
+  }
+  if (seg.type === 'ride') {
+    return {
+      type: seg.type,
+      time: seg.times.durations.total,
+      route: seg.route.name === undefined ? seg.route.key : seg.route.name,
+    }
+  }
+  if (seg.type === 'transfer') {
+    return {
+      type: seg.type,
+      time: seg.times.durations.total,
+      stop1Num: seg.from.stop.key,
+      stop1Name: seg.from.stop.name,
+      stop2Num: seg.to.stop.key,
+      stop2Name: seg.to.stop.name,
+    }
+  }
+}
